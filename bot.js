16f4ba5c58b6c9e41fd0e55d5f9538a6b311cc59
Bebecactus <34408111+Bebecactus@users.noreply.github.com>
2017-12-10 04:36:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(Mzg4NDYwNTg2MTY3Njk3NDE5.DQ484A.cCbjqUekXaz7NpVNbjxqPm5Ckuk);
